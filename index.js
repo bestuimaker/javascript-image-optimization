7990c4e083deafaa4c2f09e5d76d0a1ed4e353d9
Bhagwati Pandey <Bhagwati.Pandey@NIIT-Tech.com>
2021-12-31 15:22:33 +0530

    app reference corrected

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT;
 
 var serve = serveStatic("./");
 
-var server = http.createServer(function(req, res) {
+var app = http.createServer(function(req, res) {
   var done = finalhandler(req, res);
   serve(req, res, done);
 });
